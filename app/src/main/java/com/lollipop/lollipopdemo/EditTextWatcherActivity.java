9b978ec8ed341c6a75a231c7b00e9f884a604f05
lollipop <976731597@qq.com>
2019-05-29 15:17:14 +0800

    lollipop : editText 文本监听事件的处理

---
+++ app/src/main/java/com/lollipop/lollipopdemo/EditTextWatcherActivity.java
@@ -0,0 +1,13 @@
+package com.lollipop.lollipopdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditTextWatcherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text_watcher);
+    }
+}
