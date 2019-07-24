1bd1de5b9258648cc5e79137acc7ac01a1d685d5
lollipop <976731597@qq.com>
2019-07-24 15:07:00 +0800

    [算法] 冒泡排序

---
+++ app/src/main/java/com/lollipop/lollipopdemo/Algorithm/BubbleSort.java
@@ -0,0 +1,35 @@
+package com.lollipop.lollipopdemo.Algorithm;
+
+/**
+ * 冒泡排序
+ */
+public class BubbleSort {
+    private static int[] array = new int[]{3, 1, 5, 4, 8};
+
+    public static void main(String[] args) {
+        sort(array);
+        log();
+    }
+
+    private static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            boolean flagBreak = false;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                int temp = array[j];
+                if (array[j] > array[j + 1]) {
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    flagBreak = true;
+                }
+            }
+            if (!flagBreak) //说明数据无交换，提前退出
+                break;
+        }
+    }
+
+    private static void log() {
+        for (int i1 : array) {
+            System.out.println(i1);
+        }
+    }
+}
